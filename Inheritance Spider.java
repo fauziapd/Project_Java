faf3c916fe48232a0dfb64f3f13018932c883f63
fauziapd <fauziapd@gmail.com>
2015-04-12 20:59:38 +0700

    Create Inheritance Spider.java

---
+++ Inheritance Spider.java
@@ -0,0 +1,12 @@
+package animal;
+
+/**
+ *
+ * @author Fauzia Puspita Dewi (0613U042)
+ */
+public class Spider extends Animal{
+ 
+    public Spider(){
+        System.out.println("\nSPIDER");
+    }
+}
